54d9e6b4f3d5aafa53dd60ddc340e2a0e8ff4ece
logarithm <iamrohan04@gmail.com>
2019-10-31 16:52:37 +0530

    added route add

---
+++ models/Route.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+var RouteSchema = new Schema({
+  startDest: {
+    type: String,
+    required: true
+  },
+  finishDest: {
+    type: String,
+    required: true
+  },
+  startDestCode: {
+    type: String,
+    required: true
+  },
+  finishDestCode: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = Route = mongoose.model("myRoutes", RouteSchema);
--- routes/api/flight.js
+++ routes/api/flight.js
@@ -11,9 +11,53 @@ router.use(bodyparser.urlencoded({ extended: false }));
 router.use(bodyparser.json());
 
 const Flight = require("./../../models/Flight");
-
+const Route = require("./../../models/Route");
 router.get("/", (req, res) => {
   res.json({ sucess: true, message: "Flight Module" });
 });
+
+router.post(
+  "/addflight",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // // res.json(req.user);
+    console.log(req.user.roleID);
+    if (req.user.roleID == "2") {
+      const flight = {};
+      flight.name = req.body.name;
+      flight.flightNumber = req.body.flightNumber;
+      var flightSchema = new Flight(flight);
+      flightSchema
+        .save()
+        .then(flight => res.json({ status: true, payload: flight }))
+        .catch(err => res.json({ status: false, message: err }));
+    } else {
+      res.json({ status: false, message: "Not an admin" });
+    }
+  }
+);
+
+router.post(
+  "/addroute",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // // res.json(req.user);
+    console.log(req.user.roleID);
+    if (req.user.roleID == "2") {
+      const route = {};
+      route.startDest = req.body.startDest;
+      route.finishDest = req.body.finishDest;
+      route.startDestCode = req.body.startDestCode;
+      route.finishDestCode = req.body.finishDestCode;
+      var routeSchema = new Route(route);
+      routeSchema
+        .save()
+        .then(route => res.json({ status: true, payload: route }))
+        .catch(err => res.json({ status: false, message: err }));
+    } else {
+      res.json({ status: false, message: "Not an admin" });
+    }
+  }
+);
 
 module.exports = router;
