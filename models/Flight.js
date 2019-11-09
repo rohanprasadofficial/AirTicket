c96df0ce1fd40d92aa615bffe529d963103e4f28
logarithm <iamrohan04@gmail.com>
2019-11-09 11:27:40 +0530

    changes flight schema

--- models/Flight.js
+++ models/Flight.js
@@ -15,6 +15,54 @@ var FlightSchema = new Schema({
     type: String,
     required: true
   },
+  Source: {
+    type: String,
+    required: true
+  },
+  Destination: {
+    type: String,
+    required: true
+  },
+
+  sourceTime: {
+    type: String,
+    required: true
+  },
+
+  destTime: {
+    type: String,
+    required: true
+  },
+
+  sourceDate: {
+    type: String,
+    required: true
+  },
+
+  DestDate: {
+    type: String,
+    required: true
+  },
+
+  FclassPrice: {
+    type: String,
+    required: true
+  },
+
+  BclassPrice: {
+    type: String,
+    required: true
+  },
+
+  PclassPrice: {
+    type: String,
+    required: true
+  },
+
+  EclassPrice: {
+    type: String,
+    required: true
+  },
 
   date: {
     type: Date,
